93befc91aa113013b117729e7e41f8e1f15bbe01
Rumman <chowdhuryrumman78@gmail.com>
2023-12-11 00:04:19 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,46 @@
+import React, { Suspense } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+const ShowData = React.lazy(() => import("./pages/ShowData"));
+const AddTransaction = React.lazy(() => import("./pages/AddTransaction"));
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtedtedRoutes>
+              <Suspense>
+                <AddTransaction />
+              </Suspense>
+            </ProtedtedRoutes>
+          }
+        />
+        <Route
+          path="/show-data"
+          element={
+            <ProtedtedRoutes>
+              <Suspense>
+                <ShowData />
+              </Suspense>
+            </ProtedtedRoutes>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
+};
+export function ProtedtedRoutes(props) {
+  if (localStorage.getItem("user")) {
+    return props.children;
+  } else {
+    return <Navigate to="/login" />;
+  }
+}
+
+export default App;
